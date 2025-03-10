8b02458c946c525a94284904e4e2656e6b41b78e
MadMike0204 <1724715245@qq.com>
2025-03-10 17:56:20 +0800

    Updated at 2025/3/10/evening

--- silicon-course/src/components/Person.vue
+++ silicon-course/src/components/Person.vue
@@ -1,26 +1,43 @@
 <template>
   <!--  // 组件结构-->
   <div class="person">
-    <p>{{ sum }}</p><br>
-    <button @click="changeSum">改变sum</button>
+    <h3>姓名:{{person.name}}</h3>
+    <h3>年龄:{{person.age}}</h3>
+    <h3>公司:{{person.inc}}</h3>
+    <button @click="changeAge">更改年龄</button>
+    <button @click="changeInc">更改公司</button>
+    <button @click="stopWatchSJ">停止石坚</button>
+    <button @click="changePerson">换成李四</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person2333">
 import { ref, watch } from 'vue'
-
-  let sum = ref(-2);
-  function changeSum(){
-    sum.value++;
-  }
-  const stopWatch = watch(sum, (newVal, oldVal) => {
-    console.log("发生变化"+newVal+" "+oldVal);
-    if(newVal>=10){
-      stopWatch(newVal);
-    }
+  let person = ref({
+    name:"张三",
+    age: 18,
+    inc: "Joseph Inc."
   })
-  console.log(stopWatch);
-  console.log(watch(sum, (newVal, oldVal) => {}))
+  function changeAge() {
+    person.value.age += 1;
+  }
+  function changeInc() {
+    person.value.inc += "~";
+  }
+  function changePerson(){
+    person.value = {
+      name:"李四",
+      age:188,
+      inc: "Joseph Inc."
+    }
+  }
+  const stopWatch = watch(person, (newValue,oldValue) => {
+    console.log("Person改变了",newValue,oldValue);
+  },{ immediate: true,deep: true })
+  function stopWatchSJ(){
+    stopWatch();
+  }
+
 </script>
 
 <style scoped>
